339d729a8c677c0bda1d4cd470a1bf4b897aaa2b
Divini-sandhya <divinisandhya@gmail.com>
2025-01-18 19:33:39 +0530

    Updated project structure and .gitignore

---
+++ frontend/src/test/java/com/sandhya/portfolio_tracker_backend/PortfolioTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandhya.portfolio_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
